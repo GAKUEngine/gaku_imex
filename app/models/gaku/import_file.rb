5c7c00a3a80074b9b334b209da3840e22bb50eb8
Georgi Tapalilov <tapalilov@gmail.com>
2014-05-29 17:06:22 +0300

    [model] add validations import file

--- app/models/gaku/import_file.rb
+++ app/models/gaku/import_file.rb
@@ -1,5 +1,11 @@
 module Gaku
   class ImportFile < ActiveRecord::Base
+
+    validates :data_file, :importer_type, presence: true
+
     has_attached_file :data_file
+
+    Types = %w( students )
+
   end
 end
