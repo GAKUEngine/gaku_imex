dcaa36cb3aa8a7b936557b07eb8cc80528326c96
Georgi Tapalilov <tapalilov@gmail.com>
2014-05-29 17:04:13 +0300

    [factory] for import file; didn work

---
+++ spec/factories/import_file_factory.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :import_file, class: Gaku::ImportFile do
+    importer_type 'students'
+    data_file do
+      fixture_file_upload(Rails.root + '../support/student.csv',
+                          'text/csv')
+    end
+  end
+
+end
