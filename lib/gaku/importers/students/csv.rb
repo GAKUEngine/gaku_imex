9b41c48fe5dfd2b3e7bb4ee8a779754d05f406b3
影月 零 <zero@genshin.org>
2014-06-03 16:15:51 +0900

    quick WIP patch to student CSV

--- lib/gaku/importers/students/csv.rb
+++ lib/gaku/importers/students/csv.rb
@@ -15,6 +15,7 @@ module Gaku::Importers::Students
 
       ActiveRecord::Base.transaction do
         @csv.each_with_index do |row|
+          next if check_registration(row)
           student = Gaku::Student.new(row)
           student.enrollment_status_code = enrollment_status_code
           if student.save
@@ -27,5 +28,10 @@ module Gaku::Importers::Students
       students
     end
 
+    def check_registration(row)
+      return true if row.id.nil? || row.id == ""
+      return true if Gaku::Student.exists?(foreign_id_code: row.foreign_id_code)
+      false
+    end
   end
 end
