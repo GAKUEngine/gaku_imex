65e5b60bd3f7a5520716ce002b5b6bb72dfeffd1
Georgi Tapalilov <tapalilov@gmail.com>
2014-07-09 15:53:52 +0300

    [override] add reports menu to students

---
+++ app/overrides/reports.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(virtual_path: 'gaku/students/edit',
+                     name: 'importer_menu',
+                     insert_bottom: '#accordion',
+                     partial: 'gaku/students/reports/report_link',
+                     disabled: false
+                     )
