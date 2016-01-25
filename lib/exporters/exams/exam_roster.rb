2e2a754c678c39429313e4a943953f66d1fd8a09
snowsunny <snowsunny1107@gmail.com>
2016-01-25 14:02:15 +0900

    fix code & update exam_roster.tlf

--- lib/exporters/exams/exam_roster.rb
+++ lib/exporters/exams/exam_roster.rb
@@ -16,9 +16,14 @@ module Gaku
               page.list("exam_roster").add_row do |row|
                 row.item(:index_number).value(student.id)
                 row.item(:examinee_number).value(student.id)
-                row.item(:examinee_name).value(student.name)
-                row.item(:examinee_name_reading).value(student.name_reading)
-                row.item(:origin_junior_high).value(student.id)
+                row.item(:examinee_name).value student.surname + ' ' + student.name
+                row.item(:examinee_name_reading).value student.surname_reading + ' ' + student.name_reading
+                if student.external_school_records_count
+                  school_name = student.external_school_records.last.school.name
+                else
+                  school_name = 'none'
+                end
+                row.item(:origin_junior_high).value school_name
                 row.item(:attendance).value(student.id)
               end
             end
